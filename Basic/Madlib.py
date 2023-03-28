1bcb93e61b4d31701475e48a4c036aa0a47d9fc4
Egriffith8368 <125297215+Egriffith8368@users.noreply.github.com>
2023-03-27 23:17:36 -0400

    Create Madlib.py

---
+++ Basic/Madlib.py
@@ -0,0 +1,38 @@
+# Questions for the user to answer
+
+place = input('Choose a place: ')
+
+p_animal = input('Choose a plural animal: ')
+
+verb = input('Choose a verb: ')
+
+food = input('Name a food: ')
+
+ad = input('Choose an adjective (Describing word): ')
+
+ad2 = input('Choose an adjective (Describing word): ')
+
+person = input('Choose a person:')
+
+occupation = input('Choose a occupation:')
+
+ad3 = input('Chose a adjective (Descibing word):')
+
+noun = input('Choose a noun:')
+
+# Print a story from the user input
+
+print('------------------------------------------')
+
+print('Welcome to the planet of ', place, 'where ', p_animal ,'run around and ', verb)
+
+print('On this planet trees are made of ', food, ' and flowers smell like ', ad)
+
+print('People here are very ', ad2, '. The people are fans of ', person, 'the ', occupation)
+
+print('This planets weather is very', ad3)
+
+print('Many buildings are made of ', noun)
+
+import time
+time.sleep(100)
