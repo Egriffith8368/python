b0a0637ebcf2019e4c07d6e7eb618fca04369456
Egriffith8368 <125297215+Egriffith8368@users.noreply.github.com>
2023-03-29 10:48:40 -0600

    Create spyro2.py

---
+++ spyro2.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+
+def spyro():
+    speed(0)
+    for v in range(1000):
+    
+        for x in range(4):
+            forward(50)
+            circle(100,20)
+            left(180)
+            right(90)
+            left(180)
+            backward(10)
+        left(12053)
+
+
+
+
+
+
+
+spyro()
